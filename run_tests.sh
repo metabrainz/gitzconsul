2c73ca0f5f19567cc6baa11ba20bb593382f5ff8
Laurent Monin <github@norz.org>
2021-11-06 11:36:50 +0100

    Also pylint tests directory

--- run_tests.sh
+++ run_tests.sh
@@ -2,4 +2,4 @@
 
 poetry run python -m pytest -v --cov=gitzconsul tests/
 poetry run flake8 --show-source --statistics --count
-poetry run pylint -j0 --exit-zero gitzconsul
+poetry run pylint -j0 --exit-zero gitzconsul tests
